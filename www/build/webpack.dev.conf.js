e38b75c8bb837b3db88ea27875c6e93e6f3a3a41
oconnill <doconnelldesign@gmail.com>
2018-02-27 21:09:06 -0700

    d

--- www/build/webpack.dev.conf.js
+++ www/build/webpack.dev.conf.js
@@ -19,9 +19,10 @@ module.exports = merge(baseWebpackConfig, {
   devtool: '#cheap-module-eval-source-map',
   plugins: [
     new webpack.DefinePlugin({
-      PRODUCTION: JSON.stringify(PRODUCTION),
-      DEVELOPMENT: JSON.stringify(DEVELOPMENT)
-  }),
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
